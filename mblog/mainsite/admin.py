04c10a7098fcf1ce865d67ab1bcd6557c593fd67
hlc23 <henryleecode23@gmail.com>
2025-03-21 02:59:41 +0800

    HW3

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Phone
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Phone)
--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -13,4 +13,32 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Phone(models.Model):
+    RAM_CHOICES = [
+        ('2GB', '2GB'),
+        ('4GB', '4GB'),
+        ('6GB', '6GB'),
+        ('8GB', '8GB'),
+    ]
     
+    ROM_CHOICES = [
+        ('16GB', '16GB'),
+        ('32GB', '32GB'),
+        ('64GB', '64GB'),
+        ('128GB', '128GB'),
+    ]
+    
+    name = models.CharField(max_length=200)
+    intro = models.TextField()
+    ram = models.CharField(max_length=10, choices=RAM_CHOICES)
+    rom = models.CharField(max_length=10, choices=ROM_CHOICES)
+    price = models.IntegerField()
+    image = models.CharField(max_length=500, blank=True, help_text="Enter a URL for the phone image")
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-pub_date',)
+    
+    def __str__(self):
+        return self.name
